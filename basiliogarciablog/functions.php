39d4934398d92667a97a52d2790b7dfc6619c4f6
Basilio García <basiliog80@gmail.com>
2024-07-10 20:40:03 +0200

    menus

---
+++ basiliogarciablog/functions.php
@@ -0,0 +1,69 @@
+<?php
+
+/*
+function basiliogarciablog_style() {
+    wp_enqueue_style( 'main', get_stylesheet_uri(), array('normalize'), '1.0.0' );
+    wp_enqueue_style( 'normalize', 'https://necolas.github.io/normalize.css/8.0.1/normalize.css', array(), '8.0.1');
+}
+
+add_action('wp_enqueue_scripts', 'basiliogarciablog_style');
+*/
+
+function basiliogarciablog_menu() {
+    register_nav_menus( array(
+        'main' => __('Menú principal', 'basiliogarciablog'),
+    ));
+}
+
+add_action('init', 'basiliogarciablog_menu');
+
+function basiliogarcia_filter_menu_li_id($menu_id, $item, $args, $depth) {
+    // Verifica si el menú es el 'main'
+    if ($args->theme_location == 'main') {
+        return ''; // Elimina el id de los <li>
+    }
+    return $menu_id;
+}
+add_filter('nav_menu_item_id', 'basiliogarcia_filter_menu_li_id', 10, 4);
+
+function basiliogarcia_filter_menu_class($classes, $item, $args, $depth) {
+    if ($args->theme_location == 'main') {
+        // Limpia todas las clases
+        $new_classes = [];
+
+        // Añade las clases personalizadas del panel de control
+        if (!empty($item->classes)) {
+            $new_classes = $item->classes;
+        }
+
+        // Quita las clases predefinidas
+        $new_classes = array_filter($new_classes, function($class) {
+            // Excluye las clases predeterminadas de WordPress
+            return !in_array($class, [
+                    'current-menu-item',
+                    'current_page_item',
+                    'menu-item',
+                    'menu-item-home',
+                    'menu-item-object-page',
+                    'menu-item-type-post_type',
+                    'page_item'
+                ]
+            );
+        });
+
+        // Quita todos los page-item-N
+        $new_classes = array_filter($new_classes, function($value) {
+            return !str_starts_with($value, 'page-item-');
+        });
+
+        // Añade la clase 'current' si el item es la página actual
+        if (in_array('current-menu-item', $item->classes)) {
+            $new_classes[] = 'current';
+        }
+
+        return $new_classes;
+    }
+
+    return $classes;
+}
+add_filter('nav_menu_css_class', 'basiliogarcia_filter_menu_class', 10, 4);
--- basiliogarciablog/index.php
+++ basiliogarciablog/index.php
@@ -1,1 +1,37 @@
-<?php
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name='robots' content='max-image-preview:large' />
+        <title>Basilio García</title>
+        <!--<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css' type='text/css' media='all' />-->
+        <link rel='stylesheet' href='<?php echo get_template_directory_uri(); ?>/style.css?ver=2.0.0' type='text/css' media='all' />
+    </head>
+    <body>
+        <header>
+            <?php
+                $args = [
+                    "theme_location" => "main",
+                    "container" => "nav",
+                    'container_id'   => '',
+                    'container_class'=> 'container',
+                    'menu_id'        => 'main-menu',
+                    'menu_class'     => 'main-menu',
+                ];
+                wp_nav_menu($args)
+            ?>
+            <!-- <img src="<?php echo get_template_directory_uri(); ?>/img/prueba.jpeg" alt=""> -->
+        </header>
+        <main>
+            <?php
+            while(have_posts()): the_post();
+                the_title();
+                the_content();
+            endwhile;
+            ?>
+        </main>
+    </body>
+</html>
+
