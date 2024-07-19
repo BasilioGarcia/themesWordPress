a6371e63aee2dc42fd37c2c0b90b2d4966f1c987
Basilio García <basiliog80@gmail.com>
2024-07-19 22:56:47 +0200

    about me

---
+++ basiliogarciablog/sections/page-aboutme.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * Template Name: About me
+ */
+get_header(); ?>
+    <div class="about-me">
+        <div class="polaroid photo1">
+            <div class="polaroid__content">
+                <img class="polaroid__img" src="https://image.ibb.co/b8UJBc/administration_architecture_big_ben_221166.jpg" alt="">
+                <span class="polaroid__caption">2024</span>
+            </div>
+        </div>
+        <div>
+            ¡Hola! Soy Basilio, "bas" cuando ya tengamos confianza, bienvenido a mi blog.
+
+
+
+             </div>
+    </div>
+
+    <ul>
+        <li>Me podéis encontrar en:</li>
+        <li>Twitter: _basiliogarcia_</li>
+        <li>YouTube: @BasilioGarcia (próximamente)</li>
+        <li>Fediverso: @basilioGarcia@tu.social</li>
+    </ul>
+<?php get_footer(); ?>
--- basiliogarciablog/sections/page-contact.php
+++
@@ -1,7 +0,0 @@
-<?php
-/*
- * Template Name: Contact
- */
-get_header(); ?>
-page-contact
-<?php get_footer(); ?>
