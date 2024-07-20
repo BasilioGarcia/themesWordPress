f1441752684443dead126d0dd6a0b333f4cfd5ac
Basilio García <basiliog80@gmail.com>
2024-07-20 22:39:06 +0200

    about me

--- basiliogarciablog/sections/page-aboutme.php
+++ basiliogarciablog/sections/page-aboutme.php
@@ -11,11 +11,28 @@ get_header(); ?>
             </div>
         </div>
         <div>
-            ¡Hola! Soy Basilio, "bas" cuando ya tengamos confianza, bienvenido a mi blog.
+            <p>
+                ¡Hola! Soy Basilio, pero puedes llamarme "Bas". ¡Bienvenido a mi blog sobre programación!.
+            </p>
+            <p>
+                Desde joven, siempre me ha apasionado el mundo de la tecnología y la programación, lo que me llevó a convertirme en programador informático.
+            </p>
+
+            <!--
+            apasionado
 
 
+            ¡Hola! Soy un orgulloso miembro de la Generación X, nacido y criado en la hermosa región de Galicia.
+            Desde joven, siempre me ha fascinado el mundo de la tecnología y la programación, lo que me llevó a convertirme en un apasionado programador informático.
+            Con el paso de los años, me especialicé como desarrollador full stack, lo que significa que tengo la habilidad de manejar tanto el desarrollo del front-end como del back-end de aplicaciones web.
+            Esto me permite tener una visión completa de cada proyecto y asegurarme de que cada detalle, desde la interfaz de usuario hasta la infraestructura del servidor, funcione a la perfección.
+            Mi carrera me ha llevado a trabajar en una variedad de proyectos emocionantes, desde pequeñas startups hasta grandes corporaciones, siempre buscando nuevas formas de innovar y mejorar la experiencia del usuario final.
+            Además de mi amor por el código, tengo una gran admiración por la cultura y las tradiciones de mi tierra natal, lo cual influye en mi trabajo y mi forma de ver el mundo.
+            A través de este blog, quiero compartir mis conocimientos, experiencias y las últimas tendencias en el desarrollo web. Espero que encuentres útil y emocionante lo que tengo para ofrecer,
+             y te invito a unirte a mí en este viaje tecnológico. ¡Gracias por visitar y espero que disfrutes del contenido!
+                -->
 
-             </div>
+        </div>
     </div>
 
     <ul>
