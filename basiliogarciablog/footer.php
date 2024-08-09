ac0ffa4e61f5323db794c580382a051a13478e95
Basilio García <basiliog80@gmail.com>
2024-08-09 20:18:12 +0200

    footer

--- basiliogarciablog/footer.php
+++ basiliogarciablog/footer.php
@@ -1,6 +1,15 @@
                 </div>
                 <footer class="footer">
-                    Footer
+                    <nav>
+                        <ul class="footer__menu">
+                            <li>
+                                <a href="#" rel="nofollow">Aviso legal</a>
+                            </li>
+                            <li>
+                                <a href="<?php echo esc_url(get_feed_link()); ?>" title="RSS">RSS</a>
+                            </li>
+                        </ul>
+                    </nav>
                 </footer>
             </div>
         </div>
